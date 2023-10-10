509b2ae34287a6c7051b82595ebf53601c37a1b8
Madhumitha <madhumithavenu03@gmail.com>
2023-10-10 10:29:45 +0530

    Creating js file

---
+++ game.js
@@ -0,0 +1,15 @@
+let randonNumber = parseInt((Math.random()*100)+1);
+
+const submit = document.querySelector('#sub');
+const input = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const ramaining = document.querySelector('.resultPress');
+const startOver = document.querySelector('#sub');
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const p = document.createElement('p');
+
+
+
+
+
