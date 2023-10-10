867157a13797bf20029c8e492df86a74f9872909
Madhumitha <madhumithavenu03@gmail.com>
2023-10-10 10:49:26 +0530

    function to guess a number

--- game.js
+++ game.js
@@ -9,6 +9,28 @@ const lowOrHi = document.querySelector('.lowOrHi');
 
 const p = document.createElement('p');
 
+let previousGuesses =[];
+let numGuesses = 1;
+let playGame = true;
+
+if(playGame){
+    submit.addEventListener('click',function(e){
+        e.preventDefault();
+        const guess = parseInt(userInput.value);
+        validGuess(guess);
+    });
+}
+function validGuess(guess){
+    if(isNaN(guess)){
+        alert('Please enter a valid number');
+    }else if(guess < 1){
+        alert('Please enter a number greater than 11');
+    }else if(guess > 100){
+        alert('Please enter a number less than 500!');
+    }
+    
+}
+
 
 
 
